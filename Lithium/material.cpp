1392d94eccccc2055c1081352941416fa23b3845
Sergey Goremykin <GSA05@mail.ru>
2015-03-05 15:13:05 +0300

    #14: Изменил класс Isotop и создал класс Mics

--- Lithium/material.cpp
+++ Lithium/material.cpp
@@ -1,11 +1,39 @@
 #include "material.h"
 
+//Mics
+Mics::Mics(QVector<quint8> grp)
+{
+    groups = grp;
+}
+
+QVector<quint8> *Mics::getGroups()
+{
+    return &groups;
+}
+
+void Mics::setGroups(QVector<quint8> grp)
+{
+    groups = grp;
+}
+
+QString Mics::save() const
+{
+    QString ans;
+    ans += QString("  %1").arg(groups.size(),3);
+    for(QVector<quint8>::const_iterator i = groups.constBegin(); i != groups.constEnd(); ++i )
+        ans += QString("%1").arg(*i,3);
+    ans += '\n';
+    return ans;
+}
+
 //Isotop
-Isotop::Isotop(quint16 num, qreal temp, qreal conc): QStandardItem()
+Isotop::Isotop(quint16 num, qreal conc, bool mod, quint16 numT, Mics mic)
 {
     number = num;
-    temperature = temp;
     concentration = conc;
+    model = mod;
+    numberT = numT;
+    micGroups = mic;
 }
 
 Isotop::~Isotop()
@@ -21,7 +49,7 @@ int Isotop::type() const
 QVariant Isotop::data(int role) const
 {
     if (role == Qt::DisplayRole) {
-        return QVariant(number);
+        return QVariant(QString("%1 (%2)").arg(number).arg(numberT));
     }
     else {
         return QStandardItem::data(role);
@@ -38,21 +66,6 @@ void Isotop::SetNumber(quint16 num)
     number = num;
 }
 
-qreal Isotop::Temperature() const
-{
-    return temperature;
-}
-
-void Isotop::SetTemperature(qreal temp)
-{
-    temperature = temp;
-}
-
-void Isotop::ChangeTemperature(qreal temp)
-{
-    SetTemperature(temp);
-}
-
 qreal Isotop::Concentration() const
 {
     return concentration;
@@ -66,6 +79,41 @@ void Isotop::SetConcentration(qreal conc)
 void Isotop::ChangeConcentration(qreal conc)
 {
     SetConcentration(conc);
+}
+
+void Isotop::ChangeTemperature(qreal temp)
+{
+
+}
+
+bool Isotop::Model() const
+{
+    return model;
+}
+
+void Isotop::SetModel(bool mod)
+{
+    model = mod;
+}
+
+quint16 Isotop::NumberT() const
+{
+    return numberT;
+}
+
+void Isotop::SetNumberT(quint16 numT)
+{
+    numberT = numT;
+}
+
+Mics Isotop::MicGroups() const
+{
+    return micGroups;
+}
+
+void Isotop::SetMicGroups(Mics mic)
+{
+    micGroups = mic;
 }
 
 //Material
--- Lithium/material.h
+++ Lithium/material.h
@@ -6,10 +6,24 @@
 #define MaterialType QStandardItem::UserType + 1
 #define IsotopType QStandardItem::UserType + 2
 
+class Mics
+{
+public:
+    Mics(QVector<quint8> grp = QVector<quint8>());
+
+    QVector<quint8> *getGroups();
+    void setGroups(QVector<quint8> grp);
+    QString save() const;
+
+private:
+    QVector<quint8> groups;
+};
+
 class Isotop: public QStandardItem
 {
 public:
-    Isotop(quint16 num = 1111, qreal temp = 300, qreal conc = 1e-7);
+    Isotop(quint16 num = 9010, qreal conc = 1e-7, bool mod = false,
+           quint16 numT = 7010, Mics mic = Mics());
     ~Isotop();
 
     int type() const;
@@ -18,17 +32,23 @@ public:
 
     quint16 Number() const;
     void SetNumber(quint16 num);
-    qreal Temperature() const;
-    void SetTemperature(qreal temp);
-    void ChangeTemperature(qreal temp);
     qreal Concentration() const;
     void SetConcentration(qreal conc);
     void ChangeConcentration(qreal conc);
+    void ChangeTemperature(qreal temp);
+    bool Model() const;
+    void SetModel(bool mod);
+    quint16 NumberT() const;
+    void SetNumberT(quint16 numT);
+    Mics MicGroups() const;
+    void SetMicGroups(Mics mic);
 
 private:
     quint16 number;
-    qreal temperature;
     qreal concentration;
+    bool model;
+    quint16 numberT;
+    Mics micGroups;
 };
 
 class Material: public QStandardItem
